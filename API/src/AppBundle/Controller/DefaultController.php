57e6f18c4ee86084ffe95a56500ec0fd9caafcd0
JOLY Sylvain <jolysylvain@MPEC-PDT-33.local>
2018-11-06 10:42:12 +0100

    base

--- API/src/AppBundle/Controller/DefaultController.php
+++ API/src/AppBundle/Controller/DefaultController.php
@@ -14,7 +14,8 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+		var_dump("test");        
+return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
