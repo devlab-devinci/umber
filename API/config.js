21ae20c65a1820e14feb9f21ef29f451b3e39bf2
azaryia <10139339+azaryia@users.noreply.github.com>
2018-12-02 15:24:58 +0100

    Update config.js

--- API/config.js
+++ API/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
   database: 'mongodb://localhost/umber',
-  token:	'HJF3278837SFYDRR09FD97JH2U1I6IU74AZ85W8X32N98B724ML92O9025TE',
+  token:	'',
   hostname:	'localhost',
   port:		'9000'
 
