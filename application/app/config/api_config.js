6b6a247b8257378d9f7a0db437a4d80b9b6ca2d1
Mélissa Loeser <melissa.loeser@gmail.com>
2019-01-19 13:42:09 +0100

    modis conf application

--- application/app/config/api_config.js
+++ application/app/config/api_config.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const API_CONFIG = {
+    version: "/v1",
     hostname: "127.0.0.1",
     protocol: "http",
-    port: "3000",
-}
+    port: 3000
+};
+
 export default API_CONFIG;
