2b5693c01d6b2a561abbf69bcdc4a6ea99bf9cf6
Mélissa Loeser <melissa.loeser@gmail.com>
2019-01-22 14:04:54 +0100

    fix cart

--- application/app/components/Cart.vue
+++ application/app/components/Cart.vue
@@ -9,25 +9,23 @@
     <scroll-view class="green">
       <ListView v-if="carts && carts.length" for="item in carts" @itemTap="" item-key="item._id">
         <v-template>
-          <GridLayout rows="auto" columns="*,*">
-            <Image v-if="item.recipient.picture && item.recipient.picture.name" col="0" row="0" :src="$config.url + '/upload/' + item.recipient.picture.name"></Image>
-            <Label :text="item.name" col="1" row="0"></Label>
-            <Label :text="item.price" col="1" row="1"/>
-            <Button text="Supprimer" col="2" @tap="removeProduct(item)" />
-          </GridLayout>
+          <StackLayout v-if="item && item.recipient && item.price && item.cartEntries.length" class="list-group-item">
+            <Image v-if="item.recipient.picture" col="0" row="0" :src="item.recipient.picture"></Image>
+            <Label :text="'Magasin : ' + item.recipient.companyName" col="1" row="0"></Label>
+            <Label v-if="(item.cartEntries && item.cartEntries.length)" :text="'Nombres d\'articles : ' + item.cartEntries.length"></Label>
+            <Label :text="'Prix total : ' + item.price.price + ' ' + item.price.devise" col="1" row="1"/>
+            <ListView v-if="item.cartEntries && item.cartEntries.length" for="entry in item.cartEntries"  @itemTap="" item-key="entry._id">
+              <v-template>
+                <StackLayout v-if="entry && entry.product">
+                  <Label :text="entry.product.name" col="1" row="0"></Label>&ndash;&gt;
+                  <Label :text="entry.price" col="1" row="1"/>
+                  <Button text="Supprimer" col="2" @tap="removeProduct(entry)" />
+                  <Image v-if="entry.product.cover && entry.product.cover.name" col="0" row="0" :src="$config.url + '/upload/' + entry.product.cover.name"></Image>
+                </StackLayout>
+              </v-template>
+            </ListView>
+          </StackLayout>
         </v-template>
-        <stackLayout>
-          <ListView v-if="item && item.cartEntries && item.cartEntries.length" for="entry in item.cartEntries" @itemTap="" item-key="entry._id">
-            <v-template>
-              <GridLayout for="entry in item.cartEntries" @itemTap="" item-key="entry._id" rows="auto" columns="*,*">
-                <Image v-if="entry.product && entry.product.cover && entry.cover.name" col="0" row="0" :src="$config.url + '/upload/' + entry.product.cover.name"></Image>
-                <Label :text="entry.product && entry.product.name" col="1" row="0"></Label>
-                <Label :text="entry.price" col="1" row="1"/>
-                <Button text="Supprimer" col="2" @tap="removeProduct(entry)" />
-              </GridLayout>
-            </v-template>
-          </ListView>
-        </stackLayout>
       </ListView>
     </scroll-view>
   </Page>
@@ -50,7 +48,6 @@
         vm.$http.get('carts', {params: {owner: vm.$store.state.currentUser._id}})
           .then(cart => {
             vm.carts = _.cloneDeep(cart.data.data);
-            console.log(vm.carts);
           })
           .catch(error => console.error(error));
       },
