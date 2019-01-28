38731f1ab19641f66f4102cd39d9c8bfe0f92af8
JOLY Sylvain <jolysylvain@MPEC-PDT-33.local>
2019-01-28 10:41:11 +0100

    CRUD category store done

--- API/routes/categoryStore.js
+++ API/routes/categoryStore.js
@@ -51,6 +51,142 @@ router.post('/category', function (req, res, next) {
  * DELETE
  * Delete category for store by id
  */
+router.delete('/category', function (req, res, next) {
+    let payload = req.body;
+
+    if (payload._id && typeof payload._id === 'string') {
+        if (payload._id.length === 24) {
+            let objectId = mongoose.Types.ObjectId(payload._id); //cast string to objectId mongoose
+
+            CategoryStore.deleteOne({_id: objectId}, function (err) {
+                if (err) {
+                    errorManager.handler(res, err, "deleteOne failed.")
+                } else {
+                    res
+                        .status(200)
+                        .json({"data": payload, "message": "deleted with success.", "status": 200})
+                }
+            });
+        } else {
+            //error invalid mongo objectId
+            errorManager.handler(res, "_id must be 24 characters to be a valid objectId", "Cannot cast _id given string to objectId mongo")
+        }
+    } else {
+        //error field empty
+        errorManager.handler(res, "_id is missing", "field _id is missing.")
+    }
+});
+
+/**
+ * PUT
+ * Update category for store by id
+ */
+router.put('/category', function (req, res, next) {
+    let payload = req.body;
+
+    if (payload.name && typeof payload.name === 'string') {
+        if (payload._id && typeof payload._id === 'string') {
+            if (payload._id.length === 24) {
+                let objectId = mongoose.Types.ObjectId(payload._id); //cast string to objectId mongoose
+
+                CategoryStore
+                    .findOne({'_id': objectId}, function (err, categoryStore) {
+                        if (err) {
+                            //cannot find one error mongo
+                            errorManager.handler(res, err, "findOne failed.")
+                        } else {
+                            if (categoryStore) {
+                                CategoryStore
+                                    .update({_id: payload._id}, {$set: {name: payload.name}}, function (err, categoryUpdated) {
+                                        if (err) {
+                                            //cannot update error mongo
+                                            errorManager
+                                                .handler(res, err, "Update failed.")
+                                        } else {
+                                            res
+                                                .status(200)
+                                                .json({
+                                                    "data": categoryUpdated,
+                                                    "message": "updated with success.",
+                                                    "status": 200
+                                                })
+                                        }
+                                    });
+                            } else {
+                                //error category not found
+                                errorManager.handler(res, ` category not found`, "category dosnt exist.")
+                            }
+                        }
+                    });
+
+            } else {
+                //error invalid mongo objectId
+                errorManager.handler(res, "_id must be 24 characters to be a valid objectId", "Cannot cast _id given string to objectId mongo")
+            }
+        } else {
+            //error field _id empty
+            errorManager.handler(res, "_id is missing", "field _id is missing.")
+        }
+    } else {
+        //error field name empty
+        errorManager.handler(res, "name is missing", "field name is missing.")
+    }
+
+});
+
+
+/**
+ * GET
+ * Find one by id
+ */
+router.get('/category/:id', function (req, res, next) {
+    let params_id = req.params.id;
+
+    if (params_id && typeof params_id === 'string') {
+        if (params_id.length === 24) {
+            let objectId = mongoose.Types.ObjectId(params_id); //cast string to objectId mongoose
+            CategoryStore
+                .findOne({'_id': objectId}, function (err, categoryStore) {
+                    if (err) {
+                        errorManager.handler(res, err, "findOne failed.")
+                    } else {
+                        res
+                            .status(200)
+                            .json({
+                                "data": categoryStore,
+                                "message": "show by id success.",
+                                "status": 200
+                            })
+                    }
+                })
+        } else {
+            errorManager.handler(res, 'invalid length _id (24 characters for valid mongoId', 'invalid _id format')
+        }
+    } else {
+        errorManager.handler(res, '_id not given expected mongoId', 'missing field _id')
+    }
+});
+
+/**
+ * GET
+ * Find all category
+ */
+router.get('/category', function (req, res, next) {
+    CategoryStore
+        .find({}, function (err, categories) {
+            if (err) {
+                errorManager.handler(res, err, "find failed.")
+            } else {
+                res
+                    .status(200)
+                    .json({
+                        "data": categories,
+                        "message": "show all success.",
+                        "status": 200
+                    })
+            }
+        })
+});
 
 
 module.exports = router;
